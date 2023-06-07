be8ebe8567c89f65b57972e15243beb1fc4b989a
anudeepvanjavakam1 <anudeepvanjavakam@gmail.com>
2023-06-07 16:15:09 -0700

    adding NER feature (token classification) to identify products

---
+++ app_ner.py
@@ -0,0 +1,280 @@
+import streamlit as st
+import pandas as pd
+from nltk import FreqDist
+
+from wordcloud import WordCloud, get_single_color_func
+import matplotlib.pyplot as plt
+
+from datetime import datetime
+
+# If we want to store app analytics in db 
+#from google.cloud import firestore 
+#from google.oauth2 import service_account
+
+import requests
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
+
+import util
+from transformers import AutoTokenizer, AutoModelForTokenClassification, Trainer
+
+import re
+
+# setting a wide layout
+st.set_page_config(layout="wide", page_icon="🔥", page_title="LIT OR NOT", initial_sidebar_state="expanded")
+
+# decorator to cache model etc. allow_output_mutation for treating model as an immutable object - a singleton 
+@st.cache_resource # 👈 Add the caching decorator
+def load_model():
+
+    model = AutoModelForTokenClassification.from_pretrained("anudeepvanjavakam/distilbert_finetuned_wnut17_wandb_ner")
+    trainer = Trainer(model=model)
+    tokenizer = AutoTokenizer.from_pretrained("anudeepvanjavakam/distilbert_finetuned_wnut17_wandb_ner")
+
+    return trainer, model, tokenizer
+
+Trainer, model, tokenizer = load_model()
+
+@st.cache_data
+def load_lottieurl(url: str): # function for lottie animations
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+lottie_url_hello = "https://assets1.lottiefiles.com/packages/lf20_onegrkmr.json"
+lottie_url_download = "https://assets10.lottiefiles.com/packages/lf20_zoe5oujy.json"
+lottie_hello = load_lottieurl(lottie_url_hello)
+lottie_download = load_lottieurl(lottie_url_download)
+
+# In case, we want to store app analytics in google's firebase storage
+#key_dict = json.loads(st.secrets["textkey"])
+#creds = service_account.Credentials.from_service_account_info(key_dict)
+#db = firestore.Client(credentials=creds, project="lit-or-not-on-reddit")
+
+COMMENT_TEMPLATE_MD = """{} - {}
+> {}"""
+
+def space(num_lines=1):
+    """Adds empty lines to the Streamlit app."""
+    for _ in range(num_lines):
+        st.write("")
+
+with st.sidebar:
+    st_lottie(lottie_hello, speed=1, height=200, key="hello_on_side")
+
+st.title('🔥Lit or Not on Reddit🔥')
+st.caption('Streamlit App by [Anudeep](https://www.linkedin.com/in/anudeepvanjavakam/)')
+
+st.write('Have you wondered 👀 if a product/platform/service is worth it or find yourself going through endless reddit posts for authentic reviews and which products are loved the most?')
+
+# create tabs
+tab1, tab2 = st.tabs(["See if something is worth it (Sentiment Analysis) 👍👎", "See products mentioned by Redditors (NER, token classification) 💬"])
+
+# counts page views, tracks all widget interactions across users
+#streamlit_analytics.start_tracking() # add ?analytics=on at the end of the app url to see app analytics
+
+## USER INPUTS ##
+st.sidebar.markdown("**Select how many posts & comments you want the app to scrape:** 👇")
+
+no_of_posts = st.sidebar.slider(label = "No. of reddit posts to scrape", min_value=1, max_value=20, value=10, step=1, format=None,
+                  key=None, help='More posts take longer time for results. Ex: "10" gets 10 most relevant posts for the search term. If the no. of posts scraped reaches this limit, then no more comments are scraped regardless of your choice for no. of comments', on_change=None, label_visibility="visible")
+
+
+no_of_comments = st.sidebar.slider(label = "No. of comments to scrape", min_value=1, max_value=500, value=100, step=1, format=None,
+                  key=None, help='More comments take longer time for results. If the no. of comments scraped reaches this limit, then no more posts are scraped regardless of your choice for no. of posts', on_change=None, label_visibility="visible")
+
+no_of_top_comments = st.sidebar.slider(label = "No. of top comments to display", min_value=1, max_value=20, value=5, step=1, format=None,
+                  key=None, help="App displays Top comments and their upvotes at the bottom of the page", on_change=None, label_visibility="visible")
+
+replies_check = st.sidebar.checkbox(label = "Include replies", value=False,
+                            help="Replies are not taken into consideration if this is not checked and only top-level comments are analyzed. Looping through multiple posts and comments and their nested replies is computationally expensive but results may be more accurate",
+                            )
+
+search_term = st.sidebar.text_input("**Enter your search term below**👇", placeholder="👉Enter here...") 
+
+button_input = st.sidebar.button("**🔥Click me to find out if its Lit🔥**", type="primary") ## button
+
+
+
+with st.sidebar:
+    st.info('For example, type "regal unlimited susbcription" or "Saatva Classic mattress reviews"🛌 or "Best 4k Tv to buy" and click the button above')
+
+if button_input:
+    with st_lottie_spinner(lottie_download, speed=1, height=200, key="download"):
+
+        # sentiment analysis tab
+        with tab1:
+
+            # get best comments from top reddit posts
+            comments, top_comments, no_of_posts, no_of_comments = util.get_comments(search_term = search_term, no_of_posts=no_of_posts, no_of_comments=no_of_comments, no_of_top_comments=no_of_top_comments, include_replies=replies_check)
+
+            if no_of_posts == 0:
+                st.warning("No posts found! Please enter another search term", icon= "⚠️")
+                st.stop()
+
+            # pre process comments and get tokens
+            lemmatized_tokens, no_of_tokens = util.pre_process_comments(comments = comments)
+
+            # apply sentiment intensity analyzer
+            df = util.apply_sentiment_analyzer(lemmatized_tokens = lemmatized_tokens, threshold = 0.10)
+            
+            # get percentage of postive and negative words in all the comments 
+            sentiment_perc_of_words = df.loc[df['label'] != 0]['label'].value_counts(normalize=True) * 100
+            
+            # if all words have positive sentiment (100%), add 0% for label '-1'
+            if sentiment_perc_of_words[1] == 100:
+                sentiment_perc_of_words[-1] = 0
+
+            # if all words have negative sentiment (100%), add 0% for label '1'
+            if sentiment_perc_of_words[-1] == 100:
+                sentiment_perc_of_words[1] = 0
+
+            perc_of_positive_words = round(sentiment_perc_of_words[1],2)
+            perc_of_negative_words = round(sentiment_perc_of_words[-1],2)
+
+            # if search term is not empty
+            if search_term!="":
+
+                # if percentage of positive words is greater than that of negative words, it is LIT
+                if perc_of_positive_words > perc_of_negative_words:
+                    st.success(f'🔥LIT!🔥 😀 Positive Sentiment: {perc_of_positive_words}%')
+                    #st.success(f'Positive Sentiment: {perc_of_positive_words}%')
+                else:
+                    st.info(f'👎NOT SO LIT... 😑 Negative Sentiment: {perc_of_negative_words}%')
+                    st.info(f'Negative Sentiment: {perc_of_negative_words}%')
+
+            col1, col2, col3 = st.columns(3)
+            col1.metric(label = 'No. of posts scraped', value = no_of_posts, delta=None, delta_color="normal", help=None, label_visibility="visible")
+            col2.metric(label = 'No. of comments scraped', value = no_of_comments, delta=None, delta_color="normal", help=None, label_visibility="visible")
+            col3.metric(label = 'No. of tokens analyzed', value = no_of_tokens, delta=None, delta_color="normal", help=None, label_visibility="visible")                
+
+            # Frequency distribution of the positive and negative words
+            frequent_pos_words = FreqDist(df.loc[df['label'] == 1].words)
+            frequent_neg_words = FreqDist(df.loc[df['label'] == -1].words)
+
+            col1, col2 = st.columns((1,1))
+
+            with col1:
+                if len(frequent_pos_words) == 0:
+                    st.warning("There are no positive words to display this chart")
+                else:            
+                    # Bar charts for most common postive words
+                    fig = util.bar_chart_for_freq_words(words_dict = frequent_pos_words, title = 'Commonly Used Positive Words Count', color = 'green', no_of_words = 20)
+                    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+            
+            with col2:
+                if len(frequent_neg_words) == 0:
+                    st.warning("There are no negative words to display this chart")
+                else:
+                    # Bar charts for most common negative words
+                    fig = util.bar_chart_for_freq_words(words_dict = frequent_neg_words, title = 'Commonly Used Negative Words Count', color = 'red', no_of_words = 20)
+                    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+            # Bar Chart for postive and negative percentage of words
+            fig = util.bar_chart_for_sentiment(perc_of_pos_words = perc_of_positive_words, perc_of_neg_words = perc_of_negative_words)
+            st.plotly_chart(fig, use_container_width=True)
+
+            #### word cloud ####
+
+            # positive words are green and negative words are red
+            color_words_dict = {
+                'green': list(df.loc[df['label'] == 1].words),
+                'red': list(df.loc[df['label'] == -1].words)
+            }
+            # neutral words are grey
+            default_color = 'grey'        
+
+            wc = WordCloud(collocations=False, background_color='white').generate_from_frequencies(frequencies=FreqDist(df.words))
+            grouped_color_func = util.SimpleGroupedColorFunc(color_words_dict, default_color)
+            wc.recolor(color_func=grouped_color_func)
+            
+            # st.pyplot without these columns would just display the image to fit 100% of the entire column width, hence stretching it.
+            # as a workaround, columns can be used to display the plot unstretched 
+            col1, col2, col3, col4, col5= st.columns([1, 1, 3, 1, 1])
+            with col3:
+                st.markdown('**Word cloud to display :green[positive], :red[negative] and neutral words**')               
+                plt.subplots(figsize=(5, 4))
+                plt.figure()
+                plt.imshow(wc, interpolation='bilinear')        
+                plt.axis('off')            
+                st.pyplot(plt)
+
+            #### end of word cloud ####
+            space()
+            
+            st.subheader('Top comments and their upvotes:')
+            st.json(top_comments)
+
+        # Named Entity Recognition (token-classification) tab for tagging products
+        with tab2:
+
+            if len(comments)==0:
+                st.error('No comments found.')
+            
+            else:
+                st.markdown("### Tagged Products")
+                st.header("")        
+
+                dfs = []
+                for comment in comments:
+                    dfs.append(util.tag_sentence(comment, tokenizer = tokenizer, model = model))
+
+                product_tokens_df = pd.concat(dfs)
+                product_tokens_df = product_tokens_df[product_tokens_df['tag'].isin(['B-product','I-product'])]
+
+                # if no product entities are found
+                if len(product_tokens_df) == 0:
+
+                    st.warning('Unfortunately, the app could not find any product mentions. Please re-phrase the search term or increase the no. of posts or comments.')
+
+                else:
+
+                    # clean and group beginning and intermediate tokens to form complete tags
+                    products_df = util.clean_product_tags(product_tokens_df)
+
+                    # center column headers
+                    #products_df = products_df.style.set_table_styles([dict(selector='th', props=[('text-align', 'center')])])
+
+                    #st.table(products_df)
+                    st.markdown(products_df.to_html(render_links=True),unsafe_allow_html=True)
+
+                    st.markdown("## Predicted probabilities for tokens")
+                    # round to two decimal places in python pandas
+
+                    # removing duplicate indexes for styling to work on the data frame
+                    product_tokens_df = product_tokens_df[~product_tokens_df.index.duplicated(keep='first')]
+
+                    # removing duplicate rows for styling to work on the data frame
+                    product_tokens_df.drop_duplicates(inplace=True)
+
+                    # display 2 decimals for probabilities
+                    pd.options.display.float_format = '{:.2f}'.format
+
+                    # style probabilities with gradient coloring 
+                    st.dataframe(product_tokens_df.style.background_gradient(subset=['probability']))
+
+    # celebratory balloons in order after displaying the results
+    st.balloons()
+
+
+st.info("""
+        This app searches reddit posts and comments across many subreddits to determine if your search term has a positive or negative sentiment based on sentiment intensity analyzer (VADER) along with product mentions using Named Entity Recognition.
+        Text in both original posts and comments is analyzed. If the results did not give you enough information, try phrasing the search term differently and be more specific.
+        Feel free to increase no. of posts and no. of comments to get more breadth and depth about what redditors think😉
+        """
+)
+
+with st.expander("Like a rhyme?", expanded=False):
+
+	st.info("""
+        Looking to buy something💰, but not sure if it's dope?👀\n
+        Let my app be your guide💪, it's the ultimate Reddit scope!🔎\n
+        No need to waste your money💲, on something that's not legit👎,\n
+        Just use this app to know if it's worth it👌\n
+        Made with love on streamlit❤,\n
+        To help you find if it's LIT or NOT on REDDIT!🔥\n
+    """)
+
+#  counts page views, tracks all widget interactions across users
+#streamlit_analytics.stop_tracking()        
---
+++ util.py
@@ -0,0 +1,327 @@
+
+import plotly.express as px
+from wordcloud import WordCloud, get_single_color_func
+import matplotlib.pyplot as plt
+import seaborn as sns
+from dotenv import load_dotenv
+import os
+
+# For gathering data from reddit
+import praw  # Python Reddit API Wrapper
+from praw.models import MoreComments
+
+import numpy as np
+import pandas as pd
+import nltk
+nltk.download('wordnet')
+nltk.downloader.download('vader_lexicon')
+import en_core_web_sm
+from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+from nltk import FreqDist
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+
+from transformers import AutoTokenizer, DataCollatorForTokenClassification, AutoModelForTokenClassification, TrainingArguments, Trainer, create_optimizer
+
+# for visualization
+sns.set(style='whitegrid', palette='Dark2')
+
+
+# loading development settings for Reddit API
+load_dotenv()
+
+# Create a reddit connection with reddit API details
+reddit = praw.Reddit(client_id=os.getenv("CLIENT_ID"),
+                     client_secret=os.getenv("CLIENT_SECRET"),
+                     user_agent=os.getenv("USER_AGENT"))
+
+
+subreddit = reddit.subreddit('all')
+
+# function to get comments
+def get_comments(search_term, no_of_posts=10, no_of_comments=20, no_of_top_comments=5, include_replies=False):
+    """function to get comments from reddit posts.
+
+    Args:
+        search_term (str): strings to search on reddit
+        no_of_posts (int, optional): number of posts to fetch. Defaults to 10.
+        no_of_comments (int, optional): number of comments to fetch. Defaults to 20.
+        no_of_top_comments (int, optional): number of comments with most upvotes to fetch. Defaults to 5.
+        include_replies (bool, optional): set True to include replies. Defaults to False.
+
+    Returns:
+        comments_body: list of all comments (including text in posts)
+        top_comments_body: a dict of of top comments (sorted by upvotes) and number of upvotes
+        len(posts): number of posts fetched
+        len(all_comments): number of comments fetched (excluding post texts)
+    """
+    posts = {}
+    all_comments = []
+    top_comments_body = {}
+
+    # Search all subreddits and get comments from top posts
+    for submission in subreddit.search(search_term, limit=no_of_posts, sort='relevance'):
+        # Get comments from each submission
+        submission.comments.replace_more(limit=None)
+
+        if include_replies:
+            comments = submission.comments.list()
+        else:
+            comments = submission.comments
+
+        # Exclude stickied posts
+        if not submission.stickied:
+            posts[submission.id] = submission.selftext
+            all_comments += comments
+
+        # If the number of comments exceeds user input, stop searching for more posts
+        if len(all_comments) >= no_of_comments:
+            break
+
+    # Sort all comments by number of upvotes
+    all_comments.sort(key=lambda comment: comment.score, reverse=True)
+
+    # Store top n comments with most upvotes
+    top_comments = all_comments[:no_of_top_comments]
+
+    # Store top comments body
+    for comment in top_comments:
+        top_comments_body[comment.body] = comment.score
+
+    # Store all comments body
+    comments_body = [comment.body for comment in all_comments]
+
+    # Add original post text to comments body
+    for text in posts.values():
+        comments_body.append(text)
+
+    return comments_body, top_comments_body, len(posts), len(all_comments)
+
+# Pre-processing comments
+def pre_process_comments(comments):
+    """function to tokenize, convert to lower case, remove stop words and lemmatize comments
+
+    Args:
+        comments (list): a list of comments (text)
+
+    Returns:
+        lemmatized_tokens: a list of pre-processed tokens
+        len(lemmatized_tokens): number to pre-processed tokens
+    """
+
+    # convert to a string object
+    # map to a list of strings
+    strings_all = [str(i) for i in comments]
+
+    # join all strings spearated by a commma
+    strings_uncleaned = ' , '.join(strings_all)
+
+    # tokenizing and cleaning strings
+    tokenizer = RegexpTokenizer('\w+|\$[\d\.]+|http\S+')
+    tokenized_string = tokenizer.tokenize(strings_uncleaned)
+
+    # converting all tokens to lowercase
+    tokenized_string_lower = [word.lower() for word in tokenized_string]
+
+    # removing stop words
+    nlp = en_core_web_sm.load()
+    all_stopwords = nlp.Defaults.stop_words
+    tokens_without_stopwords = [
+        word for word in tokenized_string_lower if not word in all_stopwords]
+
+    # stemming might return a root word that is not an actual word
+    # whereas, lemmatizing returns a root word that is an actual language word.
+    # let's normalize words via lemmatizing
+    lemmatizer = WordNetLemmatizer()
+
+    lemmatized_tokens = ([lemmatizer.lemmatize(w)
+                         for w in tokens_without_stopwords])
+
+    return lemmatized_tokens, len(lemmatized_tokens)
+
+# function to apply sentiment analyzer and get a data frame with scores and labels (based on threshold score) for each word
+def apply_sentiment_analyzer(lemmatized_tokens, threshold=0.10):
+    """function to apply sentiment analyzer to tokens
+
+    Args:
+        lemmatized_tokens (list): a list of tokens
+        threshold (float, optional): threshold to categorize compound polarity score as positive or negative. Defaults to 0.10.
+
+    Returns:
+        df: a DataFrame with columns neg (0 or 1), neu (0 or 1), pos (0 or 1), compound (polarity score), words, label (0 for neutral, 1 for positive, and -1 for negative)
+    """
+    # Applying a sentiment analyzer (VADER)
+    sia = SIA()
+    results = []
+
+    # get polarity score for words
+    for word in lemmatized_tokens:
+        pol_score = sia.polarity_scores(word)
+        pol_score['words'] = word
+        results.append(pol_score)
+
+    df = pd.DataFrame.from_records(results)
+
+    # compound score as seen above is a normalized single unideimensional measure of sentiment for a given word
+    # adding a label column to denote neutral, positive or negative sentiment with 0, 1 or -1
+    df['label'] = 0  # neutral
+    df.loc[df['compound'] > threshold, 'label'] = 1  # positive
+    df.loc[df['compound'] < -threshold, 'label'] = -1  # negative
+
+    return df
+
+# class for assigning colors to positive and negative words
+class SimpleGroupedColorFunc(object):
+    """class to assign colors to positive, negative and neutral words
+    """
+
+    def __init__(self, color_to_words, default_color):
+        """
+        Args:
+            color_to_words (dict): a dict with colors for positive and negative words as dict keys and a list of words as dict values
+            default_color (string): a color for neutral words
+        """
+        self.word_to_color = {word: color
+                              for (color, words) in color_to_words.items()
+                              for word in words}
+
+        self.default_color = default_color
+
+    def __call__(self, word, **kwargs):
+        return self.word_to_color.get(word, self.default_color)
+
+# generate bar charts for frequent words
+def bar_chart_for_freq_words(words_dict, title, color, no_of_words=20):
+    """function to generate a bar chart for frequency of words
+
+    Args:
+        words_dict (FreqDist): words and counts. Ex: FreqDist({'happy': 1, 'bad': -1, 'fan': 1, 'cheerlead': 1, 'hat': 0})
+        title (string): title for the chart
+        color (string): color for the bars
+        no_of_words (int, optional): number of most common words to display. Defaults to 20.
+
+    Returns:
+        fig: a plotly.graph_objs._figure.Figure
+    """
+    # Most common positive words
+    freq_df = pd.DataFrame(words_dict.most_common(no_of_words))
+    freq_df = freq_df.rename(
+        columns={0: "Bar Graph of Frequent Words", 1: "Count"}, inplace=False)
+
+    fig = px.bar(freq_df, x="Bar Graph of Frequent Words",
+                 y="Count", title=title, text_auto='.2s')
+    fig.update_traces(textfont_size=12, textangle=0,
+                      textposition="outside", cliponaxis=False, marker_color=color)
+
+    return fig
+
+# generate bar chart for percentage of postive, negative and neutral words
+def bar_chart_for_sentiment(perc_of_pos_words, perc_of_neg_words):
+    """function to generate a bar chart showing overall percentage of postive and negative words
+
+    Args:
+        perc_of_pos_words (int): proportion of positive words
+        perc_of_neg_words (int): proportion of negative words
+
+    Returns:
+        fig: a plotly.graph_objs._figure.Figure
+    """
+    colors = ['green', 'red']
+    fig = px.bar(x=['positive', 'negative'],
+                 y=[perc_of_pos_words, perc_of_neg_words],
+                 labels={'x': 'Sentiment', 'y': 'Percentage'},
+                 text_auto='.2s',
+                 text=[f'{perc_of_pos_words}%', f'{perc_of_neg_words}%'],
+                 title="Percentage of positive and negative words")
+    fig.update_traces(textfont_size=12, textangle=0,
+                      textposition="outside", cliponaxis=False, marker_color=colors)
+
+    return fig
+
+id2tag={0: 'O',
+        1: 'B-corporation',
+        2: 'I-corporation',
+        3: 'B-creative-work',
+        4: 'I-creative-work',
+        5: 'B-group',
+        6: 'I-group',
+        7: 'B-location',
+        8: 'I-location',
+        9: 'B-person',
+        10: 'I-person',
+        11: 'B-product',
+        12: 'I-product'}
+
+def tag_sentence(text:str, tokenizer, model):
+    """helper function that takes in a sentence, tokenizes, gets model labels and probabilities and structures them in a data frame
+    based on a given sentence, tokenizer and model
+
+    Args:
+        text (str): a sentence
+        tokenizer: A Hugging Face pre trained tokenizer (example: AutoTokenizer.from_pretrained())
+        model: A Hugging Face pre trained model (AutoModelForTokenClassification.from_pretrained())
+
+    Returns:
+        df: a data frame with columns word, tag (predicted label), and the corresponding probability
+    """
+      # convert our text to a tokenized sequence
+    inputs = tokenizer(text, truncation=True, return_tensors="pt")
+    # get outputs
+    outputs = model(**inputs)
+    # convert to probabilities with softmax
+    probs = outputs[0][0].softmax(1)
+    # get the tags with the highest probability
+    word_tags = [(tokenizer.decode(inputs['input_ids'][0][i].item()), id2tag[tagid.item()], np.round(probs[i][tagid].item() *100,2) ) 
+                    for i, tagid in enumerate (probs.argmax(axis=1))]
+
+    df=pd.DataFrame(word_tags, columns=['word', 'tag', 'probability'])
+    
+    return df
+
+def clean_product_tags(df):
+    """helper function to concatenate tokens with B-product and I-product into a single word 
+
+    Args:
+        df (pd.DataFrame): a data frame that has tokens and only product labels such as B-product or I-product 
+
+    Returns:
+        df (pd.DataFrame): a data frame that has product names and amazon links
+    """
+
+    # convert columns to a list of tuples in the format (word,tag)
+    data = list(zip(df.word, df.tag))
+    
+    combined_values = []
+    current_value = ""
+
+    # for each word, tag pair
+    for word, tag in data:
+        # If the current tag is "B-product", it indicates the start of a new value
+        if tag == "B-product":
+            if current_value:
+                # add current value to combined_values list
+                combined_values.append(current_value.strip())
+                # reset current_value
+                current_value = ""
+            # if there's nothing yet in current value, Add the beginning word to current_value
+            current_value += word
+
+        # if the current tag is "I-product", it indicates that the word is a part of an ongoing value    
+        else:
+            # add the intermediate word to current_value
+            current_value += word
+
+    # Append the last combined value if any
+    if current_value:
+        combined_values.append(current_value.strip())
+
+    df = pd.DataFrame({"Product Mentions": combined_values})
+
+    # remove any #s
+    df = df.replace("#","",regex=True)
+
+    #add amazon links
+    df['Amazon Link'] = "https://www.amazon.com/s?k="+df['Product Mentions']+"&tag=sideproject06-20"
+
+    return df
